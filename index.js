c6c68c3b6091c49dbd4c2e6b4d6558578241a078
Divyanshu Maithani <div.blackcat@gmail.com>
2018-01-08 16:26:20 +0530

    Update auth header logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
+const btoa = require('btoa');
 const app = express();
 
 /* This is where we specify options for the http-proxy-middleware
@@ -7,8 +8,13 @@ const app = express();
  * add your own backend url here */
 const options = {
     target: 'https://scalr.api.appbase.io/',
-    auth: 'cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c',
-    changeOrigin: true
+    changeOrigin: true,
+    onProxyReq: (proxyReq) => {
+        proxyReq.setHeader(
+            'Authorization',
+            `Basic ${btoa('cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c')}`
+        );
+    }
 }
 
 /* This is how we can extend this logic to do extra stuff before
