4b85efce3aed1c735fc5aae0f6b1d1e33d8943d8
Divyanshu Maithani <div.blackcat@gmail.com>
2018-04-26 11:59:08 +0530

    Update for parsing msearch requests

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 const btoa = require('btoa');
 const app = express();
+const bodyParser = require('body-parser')
 
 /* This is where we specify options for the http-proxy-middleware
  * We set the target to appbase.io backend here. You can also
@@ -9,20 +10,32 @@ const app = express();
 const options = {
     target: 'https://scalr.api.appbase.io/',
     changeOrigin: true,
-    onProxyReq: (proxyReq) => {
+    onProxyReq: (proxyReq, req) => {
         proxyReq.setHeader(
             'Authorization',
             `Basic ${btoa('cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c')}`
         );
+        /* transform the req body back from text */
+        const { body } = req;
+        if (body) {
+            if (typeof body === 'object') {
+                proxyReq.write(JSON.stringify(body));
+            } else {
+                proxyReq.write(body);
+            }
+        }
     }
 }
+
+/* Parse the ndjson as text */
+app.use(bodyParser.text({ type: 'application/x-ndjson' }));
 
 /* This is how we can extend this logic to do extra stuff before
  * sending requests to our backend for example doing verification
  * of access tokens or performing some other task */
 app.use((req, res, next) => {
-    console.log('Verifying requests ✔');
-
+    const { body } = req;
+    console.log('Verifying requests ✔', body);
     /* After this we call next to tell express to proceed
      * to the next middleware function which happens to be our
      * proxy middleware */
